571c8cc62d9566e4ae34faf95fc8e907d9530603
Deepansh Pandey <dpdurg123@gmail.com>
2025-05-19 17:41:45 +0530

    fix: update WEBRTC_SERVER_PORT to 4440 in environment configuration files

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -8,8 +8,6 @@ export const environment = {
 
 export const baseUrl = `http://localhost:3000/api/`;
 // export const baseUrl = `http://192.168.0.106:3000/api/`;
-
-
 export const webrtcServerUrl = "http://localhost:4440";
 
 // export const webrtcServerUrl = "http://192.168.0.106:4440";
